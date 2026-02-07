377bf8145323dd0ac4f579201518f584b5a014d0
NkAndre <andre270708@gmail.com>
2026-02-07 12:43:28 -0300

    feat: add novas funcionalidades

---
+++ validation.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('meuForms');
+const errorDisplay = document.getElementById('error-message');
+const modal = document.getElementById('modalSucesso');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    let mensagensErro = [];
+
+    // 1. Valida se está vazio
+    if (nome === '' || email === '' || mensagem === '') {
+        mensagensErro.push("Todos os campos precisam ser preenchidos.");
+    } 
+    //2. Valida o campo nome(mais de 3 letras)
+    else if(nome.length <3){
+        mensagensErro.push('O nome precisa de pelo menos 3 caracteres.')
+    }
+    // 3. Valida o formato do e-mail (@ e ponto)
+    else if (!emailRegex.test(email)) {
+        mensagensErro.push("Insira um e-mail espacial válido com '@'.");
+    }
+
+    // Resultado da Validação
+    if (mensagensErro.length > 0) {
+        errorDisplay.innerText = mensagensErro[0];
+        errorDisplay.style.display = 'block';
+    } else {
+        errorDisplay.style.display = 'none';
+        
+        // O modal aparece com o efeito suave do CSS
+        modal.style.display = 'flex'; 
+        
+        form.reset(); // Limpa os campos para a próxima viagem
+    }
+});
+
+function fecharModal() {
+    document.getElementById('modalSucesso').style.display = 'none';
+}
